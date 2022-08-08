074559446ee155f3a00ca4f2620221c75db5e7d8
Cedric Travelletti <ct19x463@submit02.ubelix.unibe.ch>
2022-08-08 14:36:19 +0200

    Keep pressure and bigger chunk size when convertin to ZARR.

--- scripts/convert_to_zarr.py
+++ scripts/convert_to_zarr.py
@@ -5,31 +5,33 @@ errors when computing covariances.
 """
 import os
 import sys
-from climate.utils import load_dataset
+from climate.utils import _load_dataset
 
 
 def main(base_folder, TOT_ENSEMBLES_NUMBER):
     output_path_mean = os.path.join(base_folder, "ensemble_mean.zarr")
     output_path_members = os.path.join(base_folder, "ensemble_members.zarr")
     
-    dataset_mean, dataset_members, dataset_instrumental, dataset_reference = load_dataset(
+    dataset_mean, dataset_members, dataset_instrumental, dataset_reference = _load_dataset(
             base_folder, TOT_ENSEMBLES_NUMBER)
     
-    chunk_size = 10000 # Produces squared covariance chunks of size 400MB.
+    chunk_size = 500000 # Produces squared covariance chunks of size 400MB.
     
     # Remove unused variables. Need to reset_index since zarr 
     # does not allow multi-indices. This means we have to take care about order 
     # when we unstack.
-    stacked_mean = dataset_mean.drop(
-            ['temperature', 'month']).stack(
+    stacked_mean = dataset_mean.stack(
             stacked_dim=('time', 'latitude', 'longitude')).chunk(chunk_size)
-    stacked_mean.reset_index('stacked_dim').to_zarr(output_path_mean, mode="w")
+    stacked_mean = stacked_mean.reset_index('stacked_dim')
+    print(stacked_mean)
+    stacked_mean.to_zarr(output_path_mean, mode="w")
     
-    stacked_members = dataset_members.drop(
-            ['temperature']).stack(
+    stacked_members = dataset_members.stack(
             stacked_dim=('time', 'latitude', 'longitude')).chunk(
                     {'member_nr': 1, 'stacked_dim': chunk_size})
-    stacked_members.reset_index('stacked_dim').to_zarr(output_path_members, mode="w")
+    stacked_members = stacked_members.reset_index('stacked_dim')
+    print(stacked_members)
+    stacked_members.to_zarr(output_path_members, mode="w")
     
 
 if __name__ == "__main__":
